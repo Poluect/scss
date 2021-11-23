a1993fe8d0cc02290adf8486ec5ea2e6f9635006
Dmitry Harkaviy <dmitry.harkaviy@gmail.com>
2021-11-23 15:26:36 +0200

    test

---
+++ install.sh
@@ -0,0 +1,19 @@
+set -x
+
+kubectl create namespace sscs
+kubectl -n sscs apply -f ./infrastructure/rbac/admin-role.yaml \
+            -f ./infrastructure/rbac/clusterrolebinding.yaml
+kubectl -n sscs apply -f ./infrastructure/rbac/webhook-role.yaml
+kubectl -n sscs apply -f ./ci/pipeline.yaml
+kubectl -n sscs apply -f ./ci/triggers.yaml
+
+# expose ingress for webhook
+kubectl -n sscs apply -f ./infrastructure/create-ingress.yaml
+kubectl -n sscs apply -f ./infrastructure/create-webhook.yaml
+# replate external dns
+# kubectl -n sscs apply -f ./infrastructure/ingress-run.yaml
+kubectl -n sscs apply -f ./infrastructure/ingress-nginx.yaml
+# replace github token
+kubectl -n sscs apply -f ./infrastructure/secret.yaml
+# replace github org, user, external domain
+kubectl -n sscs apply -f ./infrastructure/webhook-run.yaml
